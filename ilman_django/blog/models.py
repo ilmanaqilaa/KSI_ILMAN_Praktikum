2aa4623dc010bddb605f98b15f5a4e131589ca67
ilman <ilmanaqilaa2@gmail.com>
2022-10-27 16:47:11 +0700

    update model

--- ilman_django/blog/models.py
+++ ilman_django/blog/models.py
@@ -1,3 +1,17 @@
+from django.utils.text import slugify
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=20, validators = [])
+    body = models.TextField()
+    email = models.EmailField(blank=True)
+    slug = models.SlugField(blank=True, editable=False)
+
+    def save(self):
+        self.slug = slugify(self.title)
+        super(Post, self).save()
+        
+    # def __str__(self):
+    #     return "{}".format(self.title)
