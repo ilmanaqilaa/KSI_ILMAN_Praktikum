95c76c0a626b2fca1ce8c36ec2bb07558ef29ff0
ilman <ilmanaqilaa2@gmail.com>
2022-12-13 18:52:16 +0700

    Update Tampilan

--- ilman_django/blog/views.py
+++ ilman_django/blog/views.py
@@ -18,23 +18,6 @@ def index(request):
     print(request.GET);
     return render(request, 'blog/index.html', context)
 
-# def form(request):
-#     classform = form.classform()
-
-#     context = {
-#         'heading':'Home',
-#         'classform': classform
-#     }
-
-#     if request.method == 'POST':
-#         print("ini adalah method post")
-#         context['nama'] = request.POST['nama']
-#         context['alamat'] = request.POST['alamat']
-#         context['namac'] = request.POST['namac']
-#         context['alamatc'] = request.POST['alamatc']
-#     else:
-#         print("ini adalah method get")    
-#     return render(request, "blog/form.html", context)
 
 def recent(request):
  return HttpResponse("ini blog")
